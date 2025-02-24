fba52f2ba2055e5d6c87a7dd02b08c94df916aaf
V2kt0r <koch.viktor007@gmail.com>
2025-02-24 18:59:25 +0100

    Add title and version to live documentation

--- api/main.py
+++ api/main.py
@@ -1,9 +1,13 @@
+import os
+
 from fastapi import FastAPI
 
 from .endpoints import router
 from .middleware.process_time import add_process_time_header
 
-app = FastAPI()
+version = os.getenv("VERSION", "v0.1.0")
+
+app = FastAPI(title="BubiData", version=version)
 
 app.middleware("http")(add_process_time_header)
 
