44111d7a2363f0084472e4b1f235c72931165183
V2kt0r <koch.viktor007@gmail.com>
2025-02-24 18:01:52 +0100

    Add x-process-time header to response

--- api/main.py
+++ api/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 from .endpoints import router
+from .middleware.process_time import add_process_time_header
 
 app = FastAPI()
+
+app.middleware("http")(add_process_time_header)
 
 app.include_router(router)
