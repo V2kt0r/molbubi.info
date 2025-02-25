7b3b6adb53923dbbd32317852652004f8bf78aa9
V2kt0r <koch.viktor007@gmail.com>
2025-02-25 14:46:03 +0100

    Change data_collection to work with async db engine

--- data_collection/main.py
+++ data_collection/main.py
@@ -1,13 +1,13 @@
+import asyncio
 import os
-import time
 from datetime import UTC, datetime
 
-import requests
-import schedule
-from shared.database import Session
+import httpx
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from shared.database import AsyncSessionLocal
 from shared.logger import logger
 from shared.models import BikeModel, StationModel
-from sqlalchemy.orm import Session as SessionType
 
 from .cache_service import CacheService
 from .schemas import ApiResponse, BikeSchema, StationSchema
@@ -16,7 +16,6 @@ API_URL = os.getenv("API_URL")
 
 
 cache_service = CacheService()
-cache_service.fetch()
 
 
 def extract_relevant_data(raw_data: dict) -> list[StationSchema]:
@@ -27,16 +26,17 @@ def extract_relevant_data(raw_data: dict) -> list[StationSchema]:
     return data.countries[0].cities[0].places
 
 
-def fetch_stations() -> list[StationSchema]:
-    response = requests.get(API_URL)
-    response.raise_for_status()
-    stations = extract_relevant_data(response.json())
-    logger.debug(f"Retrieved data for {len(stations)} stations from API")
+async def fetch_stations() -> list[StationSchema]:
+    async with httpx.AsyncClient() as client:
+        response = await client.get(API_URL)
+        response.raise_for_status()
+        stations = extract_relevant_data(response.json())
+        logger.debug(f"Retrieved data for {len(stations)} stations from API")
 
-    return stations
+        return stations
 
 
-def add_new_station(session: SessionType, station: StationSchema):
+def add_new_station(session: AsyncSession, station: StationSchema):
     new_station = StationModel(
         uid=station.uid, name=station.name, lat=station.lat, lng=station.lng
     )
@@ -48,7 +48,7 @@ def add_new_station(session: SessionType, station: StationSchema):
 
 
 def add_new_bike(
-    session: SessionType,
+    session: AsyncSession,
     bike: BikeSchema,
     station: StationSchema,
     current_time: datetime,
@@ -63,14 +63,14 @@ def add_new_bike(
     logger.info(f"Bike {bike.number} moved to {station.uid}")
 
 
-def process_bikes(session: SessionType, station: StationSchema):
-    current_time = datetime.now(UTC)
+def process_bikes(session: AsyncSession, station: StationSchema):
+    current_time = datetime.now(UTC).replace(tzinfo=None)
     for bike in station.bike_list:
         if cache_service.has_bike_moved(bike, station):
             add_new_bike(session, bike, station, current_time)
 
 
-def process_stations(session: SessionType, stations: list[StationSchema]):
+def process_stations(session: AsyncSession, stations: list[StationSchema]):
     for station in stations:
         # Bikes outside stations are included in the places list.
         # The "spot" attribute for these are false
@@ -86,32 +86,30 @@ def process_stations(session: SessionType, stations: list[StationSchema]):
         process_bikes(session, station)
 
 
-def query_api_and_save():
+async def query_api_and_save():
     try:
         logger.debug("Starting API query and data save process")
 
-        session = Session()
+        async with AsyncSessionLocal() as session:
+            stations = await fetch_stations()
+            process_stations(session, stations)
 
-        stations = fetch_stations()
-        process_stations(session, stations)
-
-        session.commit()
-        logger.debug("Data saved successfully")
+            await session.commit()
+            logger.debug("Data saved successfully")
     except Exception as e:
         logger.error(f"Error in query_api_and_save: {e}", exc_info=True)
-        session.rollback()
+        await session.rollback()
     finally:
-        session.close()
+        await session.close()
 
 
-def main():
+async def main():
     logger.info("Starting data collection service")
-    schedule.every(10).seconds.do(query_api_and_save)
-
+    await cache_service.fetch()
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        await query_api_and_save()
+        await asyncio.sleep(10)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
