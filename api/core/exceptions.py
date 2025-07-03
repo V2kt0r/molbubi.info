79747e1fc214be7190f254fb8f1fb431bf1895f1
Viktor Koch <viktor.koch@zocks.io>
2025-07-03 21:59:16 +0200

    Remove everything

--- api/core/exceptions.py
+++
@@ -1,9 +0,0 @@
-from fastapi import HTTPException, status
-
-
-class BikeNotFoundException(HTTPException):
-    def __init__(self, bike_number: str):
-        super().__init__(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Bike {bike_number} not found",
-        )
--- api/schemas/distance.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class DistanceResponse(BaseModel):
-    bike_number: str
-    total_distance: float
-    distance_unit: str = "km"
-    travels: int
